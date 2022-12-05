963040f02c133e6d60d50e981d8b700c1a7b7f48
Milan Viricel <viricel.milan@gmail.com>
2022-12-05 09:01:38 +0100

    - Changement de quelques commentaires - Ajout du fichier requirements.txt

--- Base/views.py
+++ Base/views.py
@@ -3,14 +3,13 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.core.validators import URLValidator
 from django.core.exceptions import ValidationError
 from Base.models import SimplifiedUrl
-from django.views.generic import DeleteView
 
 
 def main(request):
     if request.method == 'GET':
         return render(request, 'main.html')
     elif request.method == 'POST':
-        # Recupère la variable given_url dans le template main.html
+        # Récupère la variable given_url dans le template main.html
         given_url = request.POST.get('given_url')
         url_validator = URLValidator()
         # Verification si la variable est une url valide
@@ -18,24 +17,23 @@ def main(request):
             url_validator(given_url)
         except ValidationError:
             return render(request, 'main.html', context={"is_given_url_right": False})
-        # Appel de la fonction qui créé une url aléatoire et qui n'existe pas encore
+        # Création d'une instance dans le cas ou l'url est valide en passant par le Manager
         simplified_url_object = SimplifiedUrl.objects.create_url(url_to_redirect=given_url)
         return render(request, 'main.html', context={"simplified_shortcut": simplified_url_object.redirection_shortcut})
-
     else:
         return HttpResponse("C'est interdit mon ami", status=403)
 
 
 def url_shorten(request, redirection_shortcut):
-    url = get_object_or_404(SimplifiedUrl, redirection_shortcut=redirection_shortcut)
-    return redirect(url.url_to_redirect)
+    url_object = get_object_or_404(SimplifiedUrl, redirection_shortcut=redirection_shortcut)
+    return redirect(url_object.url_to_redirect)
 
 def administration(request, redirection_shortcut=None):
     if request.method == 'GET':
         url_queryset = SimplifiedUrl.objects.all()
         if redirection_shortcut is not None:
-            url_to_delete = get_object_or_404(SimplifiedUrl, redirection_shortcut=redirection_shortcut)
-            url_to_delete.delete()
+            url_object_to_delete = get_object_or_404(SimplifiedUrl, redirection_shortcut=redirection_shortcut)
+            url_object_to_delete.delete()
             return redirect('administration')
         else:
             return render(request, "administration.html", context={"url_queryset": url_queryset})
